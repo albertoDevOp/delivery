127d4642e9b4a3640aaa19323eaca781b2fd8655
alberto rodrigues ciutad <betodios@gmail.com>
2022-02-28 00:31:50 +0100

    Old Middleware

--- src/store/middlewares/fetch/get.ts
+++ src/store/middlewares/fetch/get.ts
@@ -2,42 +2,68 @@ import { fetch as f } from "setup";
 import { Middleware, MiddlewareAPI, Dispatch } from "redux";
 import { PayloadAction } from "store";
 
-export const getMiddleware =
-  (api: any) => (next: any) => async (action:any) => {
-        // if (action.type === "SEARCH") {
+export const getMiddleware: Middleware =
+  ((api: MiddlewareAPI<Dispatch, PayloadAction>) =>
+    (next: Dispatch<PayloadAction>) =>
+      <A extends PayloadAction>(action: A): A => {
+        if (action.type === "SEARCH") {
+          const terms = action.payload
+          console.log('actionactionaction')
+          console.log(action)
 
-        //   const res = await fetch("https://621b616ffaa12ee4500c81af.mockapi.io/deliveries")
-        //   const result = await res.json();
-        //   console.log("aa", result)
+          if (terms) {
+            (async () => {
 
-        //   const b =  (  () => {
+              try {
+                console.log('2')
 
-        //         api.dispatch({
-        //           type: 'deliveries/search-results',
-        //           payload: {
-        //             loading: true,
-        //             data: { a:1 },
-        //             error: null
-        //           },
-        //         })
-                
-        //         // fetch("https://621b616ffaa12ee4500c81af.mockapi.io/deliveries").then(b => console.log(b), err => console.log(err))
+                api.dispatch({
+                  type: 'deliveries/search-results',
+                  payload: {
+                    loading: true,
+                    data: null,
+                    error: null
+                  }
+                })
+                console.log('3')
+                console.log(api.getState())
 
-        //         api.dispatch({
-        //           type: 'deliveries/decrement-results',
-        //           payload: {
-        //             loading: false,
-        //             data: null,
-        //             error: null
-        //           }
-        //         })
-        //     })
-        //     try{
-        //       b()
-        //     } catch(err) {
-        //       console.log(err)
-        //     }
-        //   }
-        
+                const response = await f("https://621b616ffaa12ee4500c81af.mockapi.io/deliveries")
+                console.log('4')
+
+                console.log(response)
+
+                api.dispatch({
+                  type: 'deliveries/search-results',
+                  payload: {
+                    loading: false,
+                    data: await response.data,
+                    error: null
+                  }
+                })
+
+                console.log(action)
+                console.log(action)
+                console.log(action)
+
+              } catch (err) {
+
+                console.log('err')
+                console.log(err)
+
+                api.dispatch({
+                  type: 'deliveries/search-results',
+                  payload: {
+                    loading: false,
+                    data: null,
+                    error: err
+                  }
+                })
+
+              }
+
+            })()
+          }
+        }
         return next(action);
-      };
+      }) as Middleware;
